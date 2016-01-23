c1819b98b6138ddceed454ea9f80039eb797c9f4
rinpopoyo <rinpopoyo@gmail.com>
2016-01-24 01:17:10 +0900

    Task 4: Room Creating ( Create Room (Controller) )

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
 
   devise_for 	:users,
   						:path => '',
   						:path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
   						:controllers => {:omniauth_callbacks => 'omniauth_callbacks',
-															 :registrations => 'registrations'
+  														 :registrations => 'registrations'
   														}
+
   resources :users, only: [:show]
+  resources :rooms
 
 end
